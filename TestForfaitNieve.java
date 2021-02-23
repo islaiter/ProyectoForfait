0e565e1706159726a9eb30c1955f7c30a0d496d5
islaiter <45145344+islaiter@users.noreply.github.com>
2021-02-23 20:03:03 +0100

    Create TestForfaitNieve.java
    Clase principal de testeo que usaremos para el proyecto, la otra contiene pruebas de metodos para ver si funcionan bien

---
+++ TestForfaitNieve.java
@@ -0,0 +1,55 @@
+package forfaitNieve;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TestForfaitNieve {
+    public static void main(String[] args) {
+
+        Scanner sn = new Scanner(System.in);
+
+        DateTimeFormatter formateadorFechaEstricto = DateTimeFormatter.ofPattern("dd-MM-uuuu", Locale.ITALIAN).withResolverStyle(ResolverStyle.STRICT);
+        ValidadorFecha validator = new ValidadorFechaLocalDate(formateadorFechaEstricto);
+
+        DateTimeFormatter formateadorFecha = DateTimeFormatter.BASIC_ISO_DATE;
+        ValidadorFecha validator2 = new ValidadorFechaLocalDate(formateadorFecha);
+
+        ForfaitNieve f1 = new ForfaitNieve();
+
+        System.out.println("Entrada");
+        System.out.println("Dia");
+        String diaEntrada = sn.nextLine();
+        System.out.println("Mes");
+        String mesEntrada = sn.nextLine();
+        System.out.println("Año");
+        String anioEntrada = sn.nextLine();
+        String fechaEntrada = "";
+        fechaEntrada = diaEntrada+"-"+mesEntrada+"-"+anioEntrada;
+        System.out.println("Salida");
+        System.out.println("Dia");
+        String diaSalida = sn.nextLine();
+        System.out.println("Mes");
+        String mesSalida = sn.nextLine();
+        System.out.println("Año");
+        String anioSalida = sn.nextLine();
+        String fechaSalida = "";
+        fechaEntrada = anioEntrada+"-"+mesEntrada+"-"+diaEntrada;
+        fechaSalida = anioSalida+"-"+mesSalida+"-"+diaSalida;
+        LocalDate fEntrada = LocalDate.parse(fechaEntrada);
+        LocalDate fSalida = LocalDate.parse(fechaSalida);
+
+
+        f1.calcularDiasTemporadaEspecial2();
+        f1.calcularDiasTemporadaAlta();
+        f1.calcularDiasTemporadaBaja();
+        f1.numeroDiasTemporadaBaja(fEntrada, fSalida);
+        f1.numeroDiasTemporadaAlta(fEntrada, fSalida);
+        f1.numeroDiasTemporadaEspecial(fEntrada, fSalida);
+
+
+
+    }
+}
