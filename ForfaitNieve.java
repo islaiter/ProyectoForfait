bc5c808ed42fe874a2cdf51f3b97480f05bc67e9
islaiter <45145344+islaiter@users.noreply.github.com>
2021-02-23 11:31:51 +0100

    Update ForfaitNieve.java

--- ForfaitNieve.java
+++ ForfaitNieve.java
@@ -1,8 +1,12 @@
 package forfaitNieve;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ForfaitNieve implements Comparable<ForfaitNieve>{
 
@@ -61,6 +65,120 @@ public class ForfaitNieve implements Comparable<ForfaitNieve>{
               System.out.println("Fechas no validas");
           }
         System.out.println("El numero de dias de temporada alta es: "+numeroDiasTemporadaAlta);
+    }
+
+    public List<LocalDate> calcularDiasTemporadaEspecial2 (){
+          List<LocalDate> listaFechas = new ArrayList<LocalDate>();
+          List<LocalDate> listaFechas2 = new ArrayList<LocalDate>();
+          List<LocalDate> listaFechas3 = new ArrayList<LocalDate>();
+
+          obtenerFechasEntreDos(LocalDate.parse("2020-12-26"),LocalDate.parse("2020-12-31"));
+          obtenerFechasEntreDos(LocalDate.parse("2021-01-02"),LocalDate.parse("2021-01-06"));
+          obtenerFechasEntreDos(LocalDate.parse("2021-02-27"),LocalDate.parse("2021-03-02"));
+
+          listaFechas = Stream.concat(obtenerFechasEntreDos(LocalDate.parse("2020-12-26"),LocalDate.
+                  parse("2020-12-31")).stream(),obtenerFechasEntreDos(LocalDate.parse("2021-01-02"),
+                  LocalDate.parse("2021-01-06")).stream()).collect(Collectors.toList());
+
+          listaFechas2 = Stream.concat(obtenerFechasEntreDos(LocalDate.parse("2021-01-02"),LocalDate.
+                parse("2021-01-06")).stream(),obtenerFechasEntreDos(LocalDate.parse("2021-02-27"),
+                LocalDate.parse("2021-03-02")).stream()).collect(Collectors.toList());
+
+          listaFechas3 = Stream.concat(listaFechas.stream(),listaFechas2.stream()).collect(Collectors.toList());
+          return listaFechas3;
+
+    }
+
+    public List<LocalDate> calcularDiasTemporadaAlta (){
+
+          /* Definimos todas las listas que nos van a hacer falta
+          periodoFechas1-4 tiene los periodos fuera de fin de semana que son dias temporada alta
+          diasTotales tiene todos los dias que van entre el 1 de Diciembre al 3 de Abril, con esta lista
+          luego calculamos que dias son de fin de semana y se añade a la lista diasFinDeSemana
+          listaDiasEspeciales llama al metodo calcularDiasTemporadaEspecial para traernos que dias son especiales
+          Por ultimo, metemos periodoFechas1-4 en listaTotal, junto con los dias de fin de semana, y al conjunto
+          luego le quitamos los dias que son dias especiales, quitamos los duplicados y ordenamos la lista
+          */
+
+          List<LocalDate> periodoFechas1 = new ArrayList<LocalDate>();
+          List<LocalDate> periodoFechas2 = new ArrayList<LocalDate>();
+          List<LocalDate> periodoFechas3 = new ArrayList<LocalDate>();
+          List<LocalDate> periodoFechas4 = new ArrayList<LocalDate>();
+          List<LocalDate> diasTotales = new ArrayList<LocalDate>();
+          List<LocalDate> diasFinDeSemana = new ArrayList<LocalDate>();
+          List<LocalDate> listaTotal = new ArrayList<LocalDate>();
+          List<LocalDate> listaDiasEspeciales = new ArrayList<LocalDate>();
+
+          //
+
+          periodoFechas1 = obtenerFechasEntreDos(LocalDate.parse("2020-12-05"),LocalDate.parse("2020-12-09"));
+          periodoFechas2 = obtenerFechasEntreDos(LocalDate.parse("2020-12-31"),LocalDate.parse("2021-01-02"));
+          periodoFechas3 = obtenerFechasEntreDos(LocalDate.parse("2021-02-06"),LocalDate.parse("2021-02-27"));
+          periodoFechas4 = obtenerFechasEntreDos(LocalDate.parse("2021-03-27"),LocalDate.parse("2021-04-05"));
+          // Desde el 1 de Diciembre, para que no pille luego como dias de temporada alta el ultimo finde de Noviembre
+          diasTotales = obtenerFechasEntreDos(LocalDate.parse("2020-12-01"),LocalDate.parse("2021-04-03"));
+          listaDiasEspeciales = calcularDiasTemporadaEspecial2();
+
+          // Recorremos todos los dias entre el 1 de Diciembre y el 3 de Abril para ver que dias son de fin de semana
+          // En caso de ser un dia de fin de semana, se añade el elemento a la lista de nombre diasFinDeSemana
+
+          for (int i=0;i<diasTotales.size();i++){
+              diasTotales.get(i).getDayOfWeek();
+              if ((diasTotales.get(i).getDayOfWeek() == DayOfWeek.SATURDAY ||
+                      diasTotales.get(i).getDayOfWeek() == DayOfWeek.SUNDAY )){
+                  diasFinDeSemana.add(diasTotales.get(i));
+              }
+          }
+
+          // Añadimos todos los elementos de las otras listas
+
+          listaTotal = Stream.concat(listaTotal.stream(),periodoFechas1.stream()).collect(Collectors.toList());
+          listaTotal = Stream.concat(listaTotal.stream(),periodoFechas2.stream()).collect(Collectors.toList());
+          listaTotal = Stream.concat(listaTotal.stream(),periodoFechas3.stream()).collect(Collectors.toList());
+          listaTotal = Stream.concat(listaTotal.stream(),periodoFechas4.stream()).collect(Collectors.toList());
+          listaTotal = Stream.concat(listaTotal.stream(),diasFinDeSemana.stream()).collect(Collectors.toList());
+
+          // Ahora eliminamos de la lista todos los elementos duplicados
+          listaTotal = listaTotal.stream().distinct().collect(Collectors.toList());
+          // Ordenamos la lista en caso de que este desordenada
+          Collections.sort(listaTotal);
+          listaTotal.removeAll(listaDiasEspeciales);
+          System.out.println("La lista FINAL de dias de temporada alta y no especial: "+listaTotal);
+
+          return listaTotal;
+
+    }
+
+    public void calcularDiasTemporadaBaja(){
+          // Los dias de comienzo y fin de la temporada, el dia de fin tiene que ser uno mayor al calendario
+          // porque el metodo obtenerFechasEntreDos obvia el ultimo dia
+          LocalDate diaInicio = LocalDate.parse("2020-11-28");
+          LocalDate diaFin = LocalDate.parse("2021-05-03");
+
+          // Definimos los dias de temporada baja y cargamos todos los dias del calendario
+          List<LocalDate> diasTemporadaBaja = new ArrayList<LocalDate>();
+          diasTemporadaBaja=obtenerFechasEntreDos(diaInicio,diaFin);
+
+          // Ahora queremos en dos listas distintas, los dias de temporada alta y especial
+          List<LocalDate> diasTemporadaAlta = new ArrayList<LocalDate>();
+          List<LocalDate> diasTemporadaEspecial = new ArrayList<LocalDate>();
+          // Cargamos los dias correspondientes, usando los metodos ya creados
+
+          diasTemporadaAlta = calcularDiasTemporadaAlta();
+          diasTemporadaEspecial = calcularDiasTemporadaEspecial2();
+
+          // Ahora a la lista de dias de temporada baja, eliminamos todos los de las otras dos listas
+           diasTemporadaBaja.removeAll(diasTemporadaAlta);
+           diasTemporadaBaja.removeAll(diasTemporadaEspecial);
+
+           // Solo nos quedaria entonces todos los dias de temporada baja
+          System.out.println("Dias de temporada baja: "+diasTemporadaBaja);
+
+    }
+
+    public List<LocalDate> obtenerFechasEntreDos (LocalDate comienzo, LocalDate fin){
+          List<LocalDate> listaDias = comienzo.datesUntil(fin).collect(Collectors.toList());
+          return listaDias;
     }
 
     public LocalDate getLocalDate(){
